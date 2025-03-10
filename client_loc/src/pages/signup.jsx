1f2f6fad98117fbd12f2bfaf1f184aceab8fc10f
dave <davechi24@gmail.com>
2025-03-10 17:09:15 +0100

    initial commit

---
+++ client_loc/src/pages/signup.jsx
@@ -0,0 +1,30 @@
+import react from "react";
+import { useState, useEffect } from "react";
+
+const Login = () =>{
+
+  // Variables
+    const [signUpData, setSignUpData] = useState({
+        userName,
+        userMail,
+        userPass,
+    })
+
+
+  // Functions
+
+
+  // UI
+  return(
+    <main id="login_Main_Cont">
+
+        <section id="login_first_sec">
+            
+        </section>
+
+    </main>
+  )
+
+}
+
+export default Login
