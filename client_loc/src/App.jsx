462372783b54516e67c40462bfb58baa4a9f642e
dave <davechi24@gmail.com>
2025-03-10 19:00:19 +0100

    signup files

---
+++ client_loc/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Signup from "./pages/signup";
+
+
+const App = () => {
+
+
+
+  // UI
+  return(
+    <main>
+        <Signup />
+    </main>
+  )
+}
+
+export default App
--- client_loc/src/pages/signup.jsx
+++ client_loc/src/pages/signup.jsx
@@ -1,25 +1,83 @@
 import react from "react";
 import { useState, useEffect } from "react";
 
-const Login = () =>{
+import '../assets/stylesheets/main.css'
+
+const Signup = () =>{
 
   // Variables
     const [signUpData, setSignUpData] = useState({
-        userName,
-        userMail,
-        userPass,
+        userName: '',
+        userMail: '',
+        userPass: '',
+        conPass: '',
     })
 
+    console.log(signUpData)
 
   // Functions
+    //  input OnChange function
+    function handleInput(e) {
+      let {id, value} = e.target
+
+      setSignUpData(prev => {
+        return { ...prev, [id]: value }
+      })
+
+    }
+
+    //  handle button click function
+    function handleBtn(e) {
+      e.preventDefault()
+      let dataS = signUpData
+      console.log(dataS) 
+    }
 
 
   // UI
   return(
-    <main id="login_Main_Cont">
+    <main id="signup_main_cont">
 
-        <section id="login_first_sec">
+        <section id="signup_first_sec">
+
+          <h1>jsijsji</h1>
+
+          <form>
+            <h2>Login</h2>
+            <div>
+              <input id="userName" placeholder="Enter Username" value={signUpData.userName} 
+                  onChange={e => handleInput(e)}
+                />
+            </div>
             
+            <div>
+              <input id="userMail" placeholder="Enter Mail" value={signUpData.userMail} 
+                  onChange={e => handleInput(e)}
+                />
+            </div>
+
+            <div>
+              <input id="userPass" placeholder="Enter Password" value={signUpData.userPass} 
+                  onChange={e => handleInput(e)}
+                />
+            </div>
+
+            <div>
+              <input id="conPass" placeholder="Confirm Password" value={signUpData.conPass} 
+                  onChange={e => handleInput(e)}
+                />
+            </div>
+
+            <button onClick={e => handleBtn(e)}>
+              Signup
+            </button>
+
+          </form>
+
+        </section>
+
+        <section id="login_second_sec">
+        
         </section>
 
     </main>
@@ -27,4 +85,4 @@ const Login = () =>{
 
 }
 
-export default Login
+export default Signup
